21daabf21c72b0cabc0824a2e33cd5c9fb8b79ab
maor2871 <ClassifiedAnonymouse@gmail.com>
2022-10-04 13:17:35 +0300

    Fish moves with arrows strokes.

--- fish_game.cpp
+++ fish_game.cpp
@@ -4,9 +4,12 @@
 using namespace std;
 
 
+// ----- Grahphics Classes -----
+
+
 class Location
 {
-    private:
+    public:
     
         int x;
         int y;
@@ -25,25 +28,17 @@ class Location
             y = new_y;
         }
         
-        int get_x() { return x; }
-        
-        int get_y() { return y; }
-        
         void set_location(Location location)
         {
-            x = location.get_x();
-            y = location.get_y();
+            x = location.x;
+            y = location.x;
         }
-        
-        void set_x(int x) { x = x; }
-        
-        void set_y(int y) { y = y; }
 };
 
 
 class Size
 {
-    private:
+    public:
         
         int width;
         int height;
@@ -62,19 +57,11 @@ class Size
             height = new_height;
         }
         
-        int get_width() { return width; }
-        
-        int get_height() { return height; }
-        
         void set_size(Size size)
         {
             width = size.width;
             height = size.height;
         }
-        
-        void set_width(int width) { width = width; }
-        
-        void set_height(int height) { height = height; }
 };
 
 
@@ -88,16 +75,28 @@ class MyGif
     */
     
     // Define the private attributes.
-    private:
+    protected:
     
         // The path to the gif.
         const char* file_path;
         
-        // The location of the gif on the screen. Reffering to top left corner.
+        // The location of the gif on the screen. Reffering to center of the gif.
         Location location;
         
         // The size of the gif on the screen.
         Size size;
+        
+        // The scale of the gif.
+        float scale;
+        
+        // The rotation of the gif.
+        float rotation;
+        
+        // If true, flips the gif horizontally.
+        bool is_flip_horizontal;
+        
+        // If true, flips the gif vertically.
+        bool is_flip_vertical;
         
         // When the gif is loaded, this variable contains the amount of frames this gif has.
         int frames_amount = 0;
@@ -115,14 +114,24 @@ class MyGif
     public:
     
         // Constructor.
-        MyGif(const char* file_path, Location new_location, Size new_size)
+        MyGif(const char* new_file_path, Location new_location, Size new_size, float new_scale = 1, float new_rotation = 0)
         {
             // Save the path to the gif file.
-            file_path = file_path;
+            file_path = new_file_path;
 
             // Save the location and size of the image.
             location.set_location(new_location);
             size.set_size(new_size);
+            
+            // Save the scale of the gif.
+            scale = new_scale;
+            
+            // Save the rotation of the gif.
+            rotation = 0;
+            
+            // Do not flip the texture on initialization.
+            is_flip_horizontal = false;
+            is_flip_vertical = false;
 
             // Create the image instance.
             my_gif_image = LoadImageAnim(file_path, &frames_amount);
@@ -130,6 +139,14 @@ class MyGif
             // Create the texture instance.
             my_gif_texture = LoadTextureFromImage(my_gif_image);        
         }
+        
+        void flip_horizontal() {is_flip_horizontal = true;}
+        
+        void flip_vertical() {is_flip_vertical = true;}
+        
+        void unflip_horizontal() {is_flip_horizontal = false;}
+        
+        void unflip_vertical() {is_flip_vertical = false;}
         
         // The function displays the next frame of the gif.
         void set_next_frame()
@@ -152,7 +169,24 @@ class MyGif
         
         void draw_next_frame()
         {
-            DrawTextureEx(my_gif_texture, (Vector2) {location.get_x(), location.get_y()}, 0, (float)size.get_width() / my_gif_texture.width, WHITE);
+            // -1 cause flip, 1 do not flips.
+            int flip_width = 1, flip_height = 1;
+            
+            // Check if required flipping.
+            if (is_flip_horizontal) {flip_width = -1;}
+            if (is_flip_vertical) {flip_height = -1;}
+            
+            // Crop the gif (we don't want to crop any gif, so just take its original frame).
+            Rectangle source = {0, 0, flip_width * my_gif_texture.width, flip_height * my_gif_texture.height};
+            
+            // Were to draw the gif. The input location is where to put the center on the screen.
+            Rectangle destination = {location.x, location.y, scale * size.width, scale * size.width};
+            
+            // We want the gif to be rotated in relation to its center, and we want that the inputed location in the destination rectangle will be the center.
+            Vector2 center = {size.width / 2, size.width / 2};
+            
+            // Draw the texture.
+            DrawTexturePro(my_gif_texture, source, destination, center, rotation, WHITE);
         }
     
         // The function removes the gif from the screen.
@@ -165,6 +199,37 @@ class MyGif
             UnloadImage(my_gif_image); 
         }
 };
+
+
+// ----- Game Classes -----
+
+
+class Fish : public MyGif
+{
+    /*
+        Represents a fish.
+    */
+    
+    private:
+    
+        // The speed of the fish on the x and y axes, pixels/frame;
+        float speed_x, speed_y;
+       
+    public:
+    
+        Fish(const char* file_path, Location new_location, Size new_size, float new_speed_x, float new_speed_y, float new_scale = 1, float new_rotation = 0) : MyGif(file_path, new_location, new_size, new_scale, new_rotation)
+        {
+            // Set the speed of the fish.
+            speed_x = new_speed_x;
+            speed_y = new_speed_y;
+        }
+        
+        void move_left() {location.x -= speed_x; flip_horizontal();}
+        void move_right() {location.x += speed_x; unflip_horizontal();}
+        void move_up() {location.y -= speed_y;}
+        void move_down() {location.y += speed_y;}
+};
+
 
 int main()
 {
@@ -194,13 +259,20 @@ int main()
     Texture2D background1 = LoadTexture(path_background1);
     
     // Load Gifs.
-    MyGif my_fish = MyGif(path_my_fish, Location(100, 100), Size(300, 300));
+    Fish my_fish = Fish(path_my_fish, Location(150, 150), Size(300, 300), 8, 3);
 	
 	// ----- Game Loop -----
 	
 	// As long as the Esc button or exit button were not pressed, continue to the next frame.
 	while (!WindowShouldClose())
 	{
+        
+        // Handle arrow keys strokes.
+        if (IsKeyDown(KEY_RIGHT)) {my_fish.move_right();}
+        if (IsKeyDown(KEY_LEFT)) {my_fish.move_left();}
+        if (IsKeyDown(KEY_UP)) {my_fish.move_up();}
+        if (IsKeyDown(KEY_DOWN)) {my_fish.move_down();}
+        
 		my_fish.set_next_frame();
 
         BeginDrawing();
